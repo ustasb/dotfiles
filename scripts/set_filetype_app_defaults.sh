6824392e078b7611ed817768de41f1b6e31da724
Brian Ustas <brianustas@gmail.com>
2017-08-12 14:48:50 -0400

    Add set_filetype_app_defaults.sh script.

---
+++ scripts/set_filetype_app_defaults.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# brew install duti
+# To get the Bundle ID of an app: mdls -name kMDItemCFBundleIdentifier -r <app_path>
+
+duti -s org.videolan.vlc .mp4 all
+duti -s org.vim.MacVim .txt all
+duti -s org.vim.MacVim .md all
+duti -s org.vim.MacVim .rb all
+duti -s org.vim.MacVim .asc all
+duti -s org.vim.MacVim .key all
