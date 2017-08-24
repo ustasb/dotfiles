d5461f07de842f23169ac76c8274864e396c440d
Brian Ustas <brianustas@gmail.com>
2017-08-24 17:35:16 -0400

    Open SVG files in Chrome.

--- scripts/set_filetype_app_defaults.sh
+++ scripts/set_filetype_app_defaults.sh
@@ -4,9 +4,12 @@
 # To get the Bundle ID of an app: mdls -name kMDItemCFBundleIdentifier -r <app_path>
 
 duti -s org.videolan.vlc .mp4 all
+
 duti -s org.vim.MacVim .txt all
 duti -s org.vim.MacVim .md all
 duti -s org.vim.MacVim .rb all
 duti -s org.vim.MacVim .asc all
 duti -s org.vim.MacVim .key all
 duti -s org.vim.MacVim .log all
+
+duti -s com.google.Chrome .svg all
