19b49da65d95aabb66f64566bb21b02e95dbb00f
Brian Ustas <brianustas@gmail.com>
2017-11-27 19:50:23 -0500

    Add bu_back_up_system.

--- scripts/shell_functions.sh
+++ scripts/shell_functions.sh
@@ -4,17 +4,24 @@ bu_source_shell() {
   source ~/.zshrc
 }
 
-# Customize the Finder sidebar defaults.
-bu_customize_finder_sidebar() {
-  ~/dotfiles/scripts/osx_finder_sidebar/main.sh
+# Update apps and install dotfiles.
+bu_update_system() {
+  ~/dotfiles/scripts/update_system.sh
+}
+
+# Back up everything at once.
+bu_back_up_system() {
+  bu_back_up_notes && \
+  bu_back_up_photo_booth && \
+  bu_back_up_1p && \
+  bu_back_up_gdrive $*
 }
 
 # Back up Brian's notes.
 bu_back_up_notes() {
   # Reason for subshell: https://stackoverflow.com/a/10382170/1575238
   (cd $USTASB_NOTES_DIR_PATH/.. \
-    && zip --quiet --recurse-paths $USTASB_UNENCRYPTED_DIR_PATH/archive/notes_backups/$(date '+%Y-%m-%d_%H-%M-%S').zip $(basename $USTASB_NOTES_DIR_PATH) \
-    && echo 'Done!')
+    && zip --quiet --recurse-paths $USTASB_UNENCRYPTED_DIR_PATH/archive/notes_backups/$(date '+%Y-%m-%d_%H-%M-%S').zip $(basename $USTASB_NOTES_DIR_PATH))
 }
 
 # Back up Google Drive contents to S3.
@@ -40,7 +47,11 @@ bu_back_up_photo_booth() {
 # Back up 1Password backups to the Cloud.
 bu_back_up_1p() {
   rsync --verbose --checksum --ignore-existing $HOME/Library/Application\ Support/1Password\ 4/Backups/* $USTASB_CLOUD_DIR_PATH/ustasb_not_encrypted/archive/1password_backups
-  echo 'Done!'
+}
+
+# Customize the Finder sidebar defaults.
+bu_customize_finder_sidebar() {
+  ~/dotfiles/scripts/osx_finder_sidebar/main.sh
 }
 
 # Build my complete journal.
@@ -63,13 +74,8 @@ bu_test_ustasb_internet_links() {
   ruby ~/dotfiles/scripts/test_ustasb_internet_links.rb
 }
 
-# Update apps and install dotfiles.
-bu_update_system() {
-  ~/dotfiles/scripts/update_system.sh
-}
-
 # Print all available terminal colors.
 # Credit: http://www.commandlinefu.com/commands/view/5876/show-numerical-values-for-each-of-the-256-colors-in-zsh
 bu_print_term_colors() {
-  for code ({000..255}) { print -nP -- "$code: %F{$code}%K{$code}Test%k%f " ; (( code % 8 && code < 255 )) || printf '\n' }
+  for code in {000..255}; do print -nP -- "$code: %F{$code}%K{$code}Test%k%f " ; (( code % 8 && code < 255 )) || printf '\n'; done
 }
