09da089285d3f8186f2a24701442ddd679cab5de
Brian Ustas <brianustas@gmail.com>
2018-03-05 16:55:31 -0500

    bu_weather: Show 5 days by default.

--- scripts/shell_functions.sh
+++ scripts/shell_functions.sh
@@ -130,7 +130,7 @@ bu_speed_test() {
 # https://github.com/jessfraz/weather
 bu_weather() {
   which weather &> /dev/null || brew install darksky-weather
-  weather -ignore-alerts -l "Cambridge, MA" $*
+  weather -ignore-alerts -l "Cambridge, MA" -d 3 $*
 }
 
 # Pomodoro timer
