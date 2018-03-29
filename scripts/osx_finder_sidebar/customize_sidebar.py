de77b5503c876ce956f87c3b1b3b5f46762aad0b
Brian Ustas <brianustas@gmail.com>
2018-03-29 12:36:15 -0400

    Update customize_sidebar.py for High Sierra.

--- scripts/osx_finder_sidebar/customize_sidebar.py
+++ scripts/osx_finder_sidebar/customize_sidebar.py
@@ -3,18 +3,18 @@ from FinderSidebarEditor import FinderSidebar
 
 sidebar = FinderSidebar()
 home_path = os.path.expanduser('~')
-gdrive_path = home_path + '/Google Drive'
+cloud_drive_path = os.environ['USTASB_CLOUD_DIR_PATH']
 unencrypted_path = os.environ['USTASB_UNENCRYPTED_DIR_PATH']
 
 sidebar.removeAll()
-sidebar.add(home_path)
-sidebar.add(unencrypted_path)
-sidebar.add(home_path + '/Desktop')
-sidebar.add(home_path + '/Downloads')
-sidebar.add(home_path + '/projects')
-sidebar.add(os.environ['USTASB_DOCS_DIR_PATH'])
-sidebar.add(unencrypted_path + '/pictures')
-sidebar.add(gdrive_path + '/ustasb_not_encrypted/ebooks')
-sidebar.add(gdrive_path)
-sidebar.add('/Applications')
 sidebar.add('/Volumes')
+sidebar.add('/Applications')
+sidebar.add(cloud_drive_path)
+sidebar.add(cloud_drive_path + '/ustasb_not_encrypted/ebooks')
+sidebar.add(unencrypted_path + '/pictures')
+sidebar.add(os.environ['USTASB_DOCS_DIR_PATH'])
+sidebar.add(home_path + '/projects')
+sidebar.add(home_path + '/Downloads')
+sidebar.add(home_path + '/Desktop')
+sidebar.add(unencrypted_path)
+sidebar.add(home_path)
