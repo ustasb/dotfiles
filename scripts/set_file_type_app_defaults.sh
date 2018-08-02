484738b329403d27539d2e21f8d22649fe656b69
Brian Ustas <brianustas@gmail.com>
2018-08-02 11:27:03 -0400

    Open CSV files with Tad.

--- scripts/set_file_type_app_defaults.sh
+++ scripts/set_file_type_app_defaults.sh
@@ -20,6 +20,8 @@ duti -s org.vim.MacVim .key all
 duti -s org.vim.MacVim .log all
 duti -s org.vim.MacVim .conf all
 
+duti -s com.antonycourtney.tad .csv all
+
 duti -s com.google.Chrome .svg all
 
 duti -s com.apple.Preview .pdf all
