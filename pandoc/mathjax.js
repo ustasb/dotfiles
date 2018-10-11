3f0880ff4148baae42579a78d5671bd37033eae0
Brian Ustas <brianustas@gmail.com>
2018-10-11 14:21:01 -0400

    Specify MathJax version.

--- pandoc/mathjax.js
+++ pandoc/mathjax.js
@@ -1,4 +1,4 @@
-var fileref=document.createElement('script')
-fileref.setAttribute("type","text/javascript")
-fileref.setAttribute("src", "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML")
-document.getElementsByTagName("head")[0].appendChild(fileref)
+var fileref=document.createElement('script');
+fileref.setAttribute("type","text/javascript");
+fileref.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-AMS-MML_HTMLorMML");
+document.getElementsByTagName("head")[0].appendChild(fileref);
