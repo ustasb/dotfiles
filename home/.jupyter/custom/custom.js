0b500f246cccf9291fd8dca136110cf6caa5fedc
Brian Ustas <brianustas@gmail.com>
2020-06-14 10:51:34 -0400

    Remove unused Jupiter notebook settings.

--- home/.jupyter/custom/custom.js
+++
@@ -1,7 +0,0 @@
-// jupyter-vim-binding
-// https://github.com/lambdalisue/jupyter-vim-binding
-require([
-  'nbextensions/vim_binding/vim_binding',
-], function() {
-  CodeMirror.Vim.map('jk', '<Esc>', 'insert');
-});
