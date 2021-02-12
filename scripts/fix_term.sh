3c06eab1efc0b7a476fa2192b67762707c616999
Brian Ustas <brianustas@gmail.com>
2021-02-12 12:14:01 -0500

    Update fix_term.sh script for Big Sur.

--- scripts/fix_term.sh
+++ scripts/fix_term.sh
@@ -1,22 +1,10 @@
 #!/bin/sh
 
-# Define terminal capabilities.
+# Define terminal capabilities
+# Copied from: https://gist.github.com/joshuarli/247018f8617e6715e1e0b5fd2d39bb6c
 # Confirm that your terminal is configured correctly with:
 # echo `tput sitm`italics`tput ritm` `tput smso`standout`tput rmso`
 
-tinfo="$(mktemp)"
-
-cat > "$tinfo" <<EOF
-xterm-256color|xterm with 256 colors and italic,
-    kbs=\177,
-    sitm=\E[3m, ritm=\E[23m,
-    use=xterm-256color,
-tmux-256color|tmux with 256 colors and italic,
-    kbs=\177,
-    sitm=\E[3m, ritm=\E[23m,
-    smso=\E[7m, rmso=\E[27m,
-    use=screen-256color,
-EOF
-
-rm -rf ~/.terminfo
-tic -xo ~/.terminfo "$tinfo"
+brew install ncurses
+/usr/local/opt/ncurses/bin/infocmp tmux-256color > ~/tmux-256color.info
+sudo tic -xe tmux-256color ~/tmux-256color.info
